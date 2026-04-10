97c755460e9c158809c30369186f60bc8fb2ab97
MehmetAksam <240205011050@student.batman.edu.tr>
2026-04-10 10:04:59 +0300

    giriş popup kaldırıldı

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function openLogin() {
+    document.getElementById("loginModal").style.display = "flex";
+}
+
+function closeLogin() {
+    document.getElementById("loginModal").style.display = "none";
+}
+
+function login() {
+    alert("Giriş yapıldı. Şimdilik deneme amaçlı çalışıyor.");
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("loginModal");
+    if (event.target === modal) {
+        closeLogin();
+    }
+};
